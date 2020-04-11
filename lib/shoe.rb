5bae98481480dfe73bc4aba1ee6caef55b5f9524
Malcom Fetus <sora.vista@gmail.com>
2020-04-11 20:53:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -18,23 +18,3 @@ class Shoe
   end
 
 end
-
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  BRANDS = []
-
-  def initialize(brand)
-    @brand = brand
-    BRANDS << brand
-  end
-
-
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
-end
